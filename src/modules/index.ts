dfe1f40ec245f636674ad8b20a7d25f87899097c
kyoongdev <9898junjun2@gmail.com>
2023-01-27 13:29:12 +0900

    feature : init post & tags modules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,5 @@
 import { GlobalModule } from './global';
+import { PostModule } from './post/post.module';
+import { TagModule } from './tag/tag.module';
 
-export const Modules = [GlobalModule];
+export const Modules = [GlobalModule, PostModule, TagModule];
