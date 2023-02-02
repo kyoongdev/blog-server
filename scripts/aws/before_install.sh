05dae3352e21edacf42db532a8080030166b0dc3
kyoongdev <9898junjun2@gmail.com>
2023-02-02 15:41:20 +0900

    chore : install packages

--- scripts/aws/before_install.sh
+++ scripts/aws/before_install.sh
@@ -7,3 +7,4 @@ if [ -d /home/ec2-user/build ]; then
     sudo rm -rf /home/ec2-user/build
 fi
 mkdir /home/ec2-user/build
+
--- scripts/aws/start_server.sh
+++ scripts/aws/start_server.sh
@@ -6,6 +6,15 @@ cd /home/ec2-user/build
 echo "cd to build File"
 source /home/ec2-user/.bash_profile
 
+npm -v
+node -v
+
+npm install -g yarn 
+
+yarn -v
+
+npm install -g pm2
+
 pm2 stop all
 pm2 kill
 
