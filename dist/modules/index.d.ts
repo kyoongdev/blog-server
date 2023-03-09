f048157d048f0b8e7ab10c2efe119adb6e8bb69a
kyoongdev <9898junjun2@gmail.com>
2023-03-09 23:30:47 +0900

    feat(db) : table

--- dist/modules/index.d.ts
+++ dist/modules/index.d.ts
@@ -1,2 +1,2 @@
-import { FileModule } from './file/file.module';
-export declare const Modules: (typeof FileModule)[];
+import { PostModule } from './post/post.module';
+export declare const Modules: (typeof PostModule)[];
---
+++ prisma/migrations/20230309143040_post_comment/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(120) NOT NULL,
+    `password` VARCHAR(250) NOT NULL,
+    `name` VARCHAR(120) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PostComment` (
+    `id` VARCHAR(191) NOT NULL,
+    `content` MEDIUMTEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NULL,
+    `postId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `PostComment` ADD CONSTRAINT `PostComment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PostComment` ADD CONSTRAINT `PostComment_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
